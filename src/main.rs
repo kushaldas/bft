571905841557da5c13b14c56b42c56ed539224c1
Kushal Das <mail@kushaldas.in>
2021-08-12 12:29:57 +0530

    Just prints debug output for an example addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use bft_types::{Instruction, Program};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let program = Program::from_file("add.bf")?;
+    dbg!(program);
+    Ok(())
 }
