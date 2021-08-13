020c94fdd10da7daa326a21db8b3a4fb40fdb0c6
Kushal Das <mail@kushaldas.in>
2021-08-13 16:47:12 +0530

    Removes extra use statements

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bft_types::{Instruction, Program};
+use bft_types::Program;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let program = Program::from_file("add.bf")?;
-    dbg!(program);
+    dbg!(program.instructions());
     Ok(())
 }
