750f9da02d86c000b139f08587a4887dee60b5c2
Kushal Das <mail@kushaldas.in>
2021-08-15 18:32:04 +0530

    Refactors according to the spec

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bft_interp::VirtualMachine;
 use bft_types::Program;
+use std::env;
 use std::error::Error;
+use std::process;
 use structopt::StructOpt;
 
 mod cli;
@@ -10,12 +12,28 @@ type GError = Box<dyn Error>;
 /// Entry point to the bft command.
 ///
 /// Requires a source code as the argument.
-fn main() -> Result<(), GError> {
+fn main() {
     let options = cli::Opt::from_args();
+    let res = run_bft(options);
+    if res.is_err() {
+        if let Ok(path) = env::current_exe() {
+            let exe = path.file_name().unwrap();
+            let msg = format!("{:?}: {}", exe, res.err().unwrap().to_string());
+            eprintln!("{}", msg);
+        }
+        process::exit(1);
+    } else {
+        // All good
+        process::exit(0);
+    }
+}
+
+/// Entry point to the bft code base
+fn run_bft(options: cli::Opt) -> Result<(), GError> {
     let filename = options.program;
     let program = Program::from_file(filename)?;
 
-    program.validate().unwrap();
+    program.validate()?;
 
     let size = options.cells.unwrap_or(0);
 
